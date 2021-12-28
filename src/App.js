e87af4f6ae5325c340242ffa9663e4d9363971e5
YuliiaAlieks <yalieksieienko@gmail.com>
2021-12-28 23:25:52 +0200

    Added slow down and speed up functionality

--- src/App.js
+++ src/App.js
@@ -7,32 +7,25 @@ import TimeDisplay from './components/TimeDisplay';
 function App() {
   const [secondsCount, setSecondsCount] = useState(0);
   const [isStarted, setIsStarted] = useState(false);
+  const [miliseconds, setMiliseconds] = useState(1000);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setSecondsCount((s) => s + 1);
-    }, 1000);
+    }, miliseconds);
 
     if (!isStarted) {
       clearInterval(intervalId);
     }
 
     return () => clearInterval(intervalId);
-  }, [isStarted]);
+  }, [isStarted, miliseconds]);
 
 
 
   function startStopHandler() {
-
     setIsStarted(!isStarted)
-    // console.log('Clicked');
-    // if (isStarted === false) {
-    //   setIsStarted(true);
-    //   console.log("🧚 ~ true", true)
-    // } else {
-    //   setIsStarted(false);
-    //   console.log("🧚 ~ false", false)
-    // }
+    console.log("🧚 ~ isStarted", isStarted)
   }
 
   function resetHandler() {
@@ -41,12 +34,35 @@ function App() {
     console.log('Reset')
   }
 
+  function slowdownHandler() {
+    if (miliseconds < 2000) {
+      setMiliseconds((ms) => ms + 100); 
+      console.log("🧚 ~ miliseconds", miliseconds)
+    } else {
+      console.log("🧚 ~ miliseconds", miliseconds)
+      return;
+    }
+  }
+
+  function speedupHandler() {
+    if (miliseconds > 100) {
+
+      setMiliseconds((ms) => ms - 100); 
+      console.log("🧚 ~ miliseconds", miliseconds)
+    } else {
+      console.log("🧚 ~ miliseconds", miliseconds)
+      return;
+    }
+  }
+
   return (
     <div className="App">
       <header>
-        <TimeDisplay secondsCount={secondsCount}/>
+        <TimeDisplay secondsCount={secondsCount} />
         <button onClick={startStopHandler}>{isStarted ? 'Stop' : 'Start'}</button>
         <button onClick={resetHandler}>Reset</button>
+        <button onClick={slowdownHandler}>Slow down</button>
+        <button onClick={speedupHandler}>Speed up</button>
       </header>
     </div>
   );
