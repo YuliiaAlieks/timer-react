fd16bc8f9d78d29841523009e7b842568b0b6fac
YuliiaAlieks <yalieksieienko@gmail.com>
2021-12-28 22:26:58 +0200

    Added Start, Stop and Reser functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((s) => s + 1);
+    }, 1000);
+
+    if (!isStarted) {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isStarted]);
+
+
+
+  function startStopHandler() {
+    console.log('Clicked');
+    if (isStarted === false) {
+      setIsStarted(true);
+      console.log("🧚 ~ true", true)
+    } else {
+      setIsStarted(false);
+      console.log("🧚 ~ false", false)
+    }
+  }
+
+  function resetHandler() {
+    setIsStarted(false);
+    setSeconds(0);
+    console.log('Reset')
+  }
+
   return (
     <div className="App">
       <header>
-        <div>00:00:00</div>
+        <div>{`00:00:${seconds}`}</div>
+        <button onClick={startStopHandler}>{isStarted ? 'Stop' : 'Start'}</button>
+        <button onClick={resetHandler}>Reset</button>
       </header>
     </div>
   );
